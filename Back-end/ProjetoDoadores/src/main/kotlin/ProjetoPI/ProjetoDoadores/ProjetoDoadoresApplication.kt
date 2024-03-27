617794fe3ebb50a33c7b2e3751569bb0af43bbc7
Danis442 <126179327+Danis442@users.noreply.github.com>
2024-03-27 15:28:04 -0300

    CRUD feito

---
+++ Back-end/ProjetoDoadores/src/main/kotlin/ProjetoPI/ProjetoDoadores/ProjetoDoadoresApplication.kt
@@ -0,0 +1,11 @@
+package ProjetoPI.ProjetoDoadores
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetoDoadoresApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetoDoadoresApplication>(*args)
+}
---
+++ Back-end/ProjetoDoadores/src/test/kotlin/ProjetoPI/ProjetoDoadores/ProjetoDoadoresApplicationTests.kt
@@ -0,0 +1,13 @@
+package ProjetoPI.ProjetoDoadores
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetoDoadoresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
