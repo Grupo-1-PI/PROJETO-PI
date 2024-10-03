edc320879e82296be6d50ab90d64e77bae0c7e23
05GabrielVaz <gabrielsantosvaz05@gmail.com>
2024-10-02 23:27:04 -0300

    merge resolvido, por favor verificar o que foi feito, pilha em agendamento concluído com sucesso!

--- Back-end/api-sangue-corinthiano/api-sangue-corinthiano/src/main/kotlin/api-sistema/school.sptech.api/repository/ConexaoApi_repositry.kt
+++ Back-end/api-sangue-corinthiano/api-sangue-corinthiano/src/main/kotlin/api-sistema/school.sptech.api/repository/ConexaoApi_repositry.kt
@@ -1,12 +1,12 @@
-
-package ProjetoPI.ProjetoDoadores.repository
-
-import ProjetoPI.ProjetoDoadores.domain.ConexaoApi_domain
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ConexaoApi_repository : JpaRepository<ConexaoApi_domain, Long> {
-    fun findAllByTipoInstituicao(tipoInstituicao: String): List<ConexaoApi_domain>
-}
-
+//
+//package ProjetoPI.ProjetoDoadores.repository
+//
+//import ProjetoPI.ProjetoDoadores.domain.ConexaoApi_domain
+//import org.springframework.data.jpa.repository.JpaRepository
+//import org.springframework.stereotype.Repository
+//
+//@Repository
+//interface ConexaoApi_repository : JpaRepository<ConexaoApi_domain, Long> {
+//    fun findAllByTipoInstituicao(tipoInstituicao: String): List<ConexaoApi_domain>
+//}
+//
