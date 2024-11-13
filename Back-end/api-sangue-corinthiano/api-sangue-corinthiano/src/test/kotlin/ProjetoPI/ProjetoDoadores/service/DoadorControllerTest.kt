57f1c5d288f49267cd7b25adb22b25ae7373278e
Danilaum <danilo.araujo@sptech.school>
2024-11-12 21:15:01 -0300

    testes unitarios

--- Back-end/api-sangue-corinthiano/api-sangue-corinthiano/src/test/kotlin/ProjetoPI/ProjetoDoadores/service/DoadorControllerTest.kt
+++ Back-end/api-sangue-corinthiano/api-sangue-corinthiano/src/test/kotlin/ProjetoPI/ProjetoDoadores/service/DoadorControllerTest.kt
@@ -1,163 +1,72 @@
-package ProjetoPI.ProjetoDoadores.service
+import ProjetoPI.ProjetoDoadores.domain.Campanha
+import ProjetoPI.ProjetoDoadores.repository.CampanhaRepository
+import ProjetoPI.ProjetoDoadores.repository.CampanhaService
+import org.junit.jupiter.api.Assertions.assertEquals
 
-import ProjetoPI.ProjetoDoadores.controller.DoadorController
-import ProjetoPI.ProjetoDoadores.domain.Doador
-import ProjetoPI.ProjetoDoadores.repository.DoadorRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.junit.jupiter.MockitoExtension
-import org.springframework.http.HttpStatus
+import org.mockito.Mockito.*
 import java.time.LocalDate
-import java.util.*
 
-@ExtendWith(MockitoExtension::class)
+
 class DoadorControllerTest {
 
-	@Mock
-	lateinit var repository: DoadorRepository
+	private lateinit var campanhaService: CampanhaService
+	private lateinit var campanhaRepository: CampanhaRepository
 
-	@InjectMocks
-	lateinit var controller: DoadorController
-
-	@Test
-	fun `test cadastrarDoador - sucesso`() {
-		val novoDoador = Doador(1, "nome", "email@test.com",LocalDate.of(1990, 1, 1), "M", true, "senha", "motivo")
-		Mockito.`when`(repository.findByEmail("email@test.com")).thenReturn(null)
-		Mockito.`when`(repository.save(novoDoador)).thenReturn(novoDoador)
-
-		val response = controller.cadastrarDoador(novoDoador)
-
-		assert(response.statusCode == HttpStatus.CREATED)
-		assert(response.body == "Cadastro realizado com sucesso!")
+	@BeforeEach
+	fun setUp() {
+		campanhaRepository = mock(CampanhaRepository::class.java)
+		campanhaService = CampanhaService(campanhaRepository)
 	}
 
 	@Test
-	fun `test cadastrarDoador - email ja cadastrado`() {
-		val novoDoador = Doador(1, "nome", "email@test.com",LocalDate.of(1990, 1, 1), "M", true, "senha", "motivo")
-		Mockito.`when`(repository.findByEmail("email@test.com")).thenReturn(novoDoador)
+	fun `test cadastrar campanha`() {
+		val campanha = Campanha(idCampanha = 1, nome = "Campanha de Doação", dtInicio = LocalDate.now(), dtFim = LocalDate.now(), idInstituicao = 1, idAdmin = 1)
+		`when`(campanhaRepository.save(campanha)).thenReturn(campanha)
 
-		val response = controller.cadastrarDoador(novoDoador)
+		val resultado = campanhaService.cadastrar(campanha)
 
-		assert(response.statusCode == HttpStatus.BAD_REQUEST)
-		assert(response.body == "Email já cadastrado!")
+		assertEquals(campanha, resultado)
+	}
+
+
+	@Test
+	fun `test transformarEmDto`() {
+		val campanha = Campanha(idCampanha = 1, nome = "Campanha de Doação", dtInicio = LocalDate.now(), dtFim = LocalDate.now(), idInstituicao = 1, idAdmin = 1)
+
+		val dto = campanhaService.transformarEmDto(campanha)
+
+		assertEquals(campanha.idCampanha, dto.idCampanha)
+		assertEquals(campanha.nome, dto.nome)
+		assertEquals(campanha.dtInicio, dto.dtInicio)
+		assertEquals(campanha.dtFim, dto.dtFim)
+		assertEquals(campanha.idInstituicao, dto.idInstituicao)
+		assertEquals(campanha.idAdmin, dto.idAdmin)
 	}
 
 	@Test
-	fun `test listarDoadores - sucesso`() {
-		val doador1 = Doador(1, "nome1", "email1@test.com",  LocalDate.of(1990, 1, 1), "M", true, "senha", "motivo")
-		val doador2 = Doador(2, "nome2", "email2@test.com", LocalDate.of(1990, 1, 1), "F", false, "senha", "motivo")
-		Mockito.`when`(repository.findAll()).thenReturn(listOf(doador1, doador2))
+	fun `test buscarUm campanha`() {
+		val campanha = Campanha(idCampanha = 1, nome = "Campanha de Doação", dtInicio = LocalDate.now(), dtFim = LocalDate.now(), idInstituicao = 1, idAdmin = 1)
+		val idEnderecoInstituicao = 1
+		`when`(campanhaRepository.existsById(idEnderecoInstituicao)).thenReturn(true)
+		`when`(campanhaRepository.findByIdCampanha(idEnderecoInstituicao)).thenReturn(campanha)
 
-		val response = controller.listarDoadores()
+		val dto = campanhaService.buscarUm(idEnderecoInstituicao)
 
-		assert(response.statusCode == HttpStatus.OK)
-		assert(response.body?.size == 2)
+		assertEquals(campanha.idCampanha, dto.idCampanha)
+		assertEquals(campanha.nome, dto.nome)
 	}
 
 	@Test
-	fun `test listarDoadores - sem conteudo`() {
-		Mockito.`when`(repository.findAll()).thenReturn(emptyList())
+	fun `test atualizar campanha`() {
+		val campanhaExistente = Campanha(idCampanha = 1, nome = "Campanha Existente", dtInicio = LocalDate.now(), dtFim = LocalDate.now(), idInstituicao = 1, idAdmin = 1)
+		val campanhaAtualizada = Campanha(idCampanha = 1, nome = "Campanha Atualizada", dtInicio = LocalDate.now(), dtFim = LocalDate.now(), idInstituicao = 1, idAdmin = 1)
+		`when`(campanhaRepository.save(campanhaAtualizada)).thenReturn(campanhaAtualizada)
 
-		val response = controller.listarDoadores()
+		val resultado = campanhaService.atualizar(1, campanhaAtualizada)
 
-		assert(response.statusCode == HttpStatus.NO_CONTENT)
-	}
-
-	@Test
-	fun `test listarDoadorPorId - sucesso`() {
-		val doador = Doador(1, "nome", "email@test.com",  LocalDate.of(1990, 1, 1), "M", true, "senha", "motivo")
-		Mockito.`when`(repository.existsById(1)).thenReturn(true)
-		Mockito.`when`(repository.findById(1)).thenReturn(Optional.of(doador))
-
-		val response = controller.listarDoadorPorId(1)
-
-		assert(response.statusCode == HttpStatus.OK)
-		assert(response.body == doador)
-	}
-
-	@Test
-	fun `test listarDoadorPorId - nao encontrado`() {
-		Mockito.`when`(repository.existsById(1)).thenReturn(false)
-
-		val response = controller.listarDoadorPorId(1)
-
-		assert(response.statusCode == HttpStatus.NOT_FOUND)
-	}
-
-	@Test
-	fun `test atualizarDoador - sucesso`() {
-		val doadorAtual = Doador(1, "nome", "email@test.com", LocalDate.of(1990, 1, 1), "M", true, "senha", "motivo")
-		val novoDoador = Doador(1, "novo nome", "novo_email@test.com",  LocalDate.of(1990, 1, 1), "F", false, "nova senha", "novo motivo")
-		Mockito.`when`(repository.existsById(1)).thenReturn(true)
-		Mockito.`when`(repository.findById(1)).thenReturn(Optional.of(doadorAtual))
-		Mockito.`when`(repository.save(doadorAtual)).thenReturn(doadorAtual)
-
-		val response = controller.atualizarDoador(1, novoDoador)
-
-		assert(response.statusCode == HttpStatus.OK)
-		assert(response.body?.nome == "novo nome")
-		assert(response.body?.email == "novo_email@test.com")
-	}
-
-	@Test
-	fun `test atualizarDoador - nao encontrado`() {
-		val novoDoador = Doador(1, "novo nome", "novo_email@test.com",  LocalDate.of(1990, 1, 1), "F", false, "nova senha", "novo motivo")
-		Mockito.`when`(repository.existsById(1)).thenReturn(false)
-
-		val response = controller.atualizarDoador(1, novoDoador)
-
-		assert(response.statusCode == HttpStatus.NOT_FOUND)
-	}
-
-	@Test
-	fun `test deletarDoador - sucesso`() {
-		Mockito.`when`(repository.existsById(1)).thenReturn(true)
-		Mockito.doNothing().`when`(repository).deleteById(1)
-
-		val response = controller.deletarDoador(1)
-
-		assert(response.statusCode == HttpStatus.OK)
-		assert(response.body == "Doador deletado com sucesso!")
-	}
-
-	@Test
-	fun `test deletarDoador - nao encontrado`() {
-		Mockito.`when`(repository.existsById(1)).thenReturn(false)
-
-		val response = controller.deletarDoador(1)
-
-		assert(response.statusCode == HttpStatus.NOT_FOUND)
-	}
-
-	@Test
-	fun `test login - sucesso`() {
-		val doador = Doador(1, "nome", "email@test.com",  LocalDate.of(1990, 1, 1), "M", true, "senha", "motivo")
-		Mockito.`when`(repository.findByEmailAndSenha("email@test.com", "senha")).thenReturn(doador)
-
-		val response = controller.login2("email@test.com", "senha")
-
-		assert(response.statusCode == HttpStatus.OK)
-		assert(response.body == "Login realizado com sucesso!")
-	}
-
-	@Test
-	fun `test login - invalido`() {
-		Mockito.`when`(repository.findByEmailAndSenha("email@test.com", "senha")).thenReturn(null)
-
-		val response = controller.login2("email@test.com", "senha")
-
-		assert(response.statusCode == HttpStatus.BAD_REQUEST)
-		assert(response.body == "Login inválido!")
-	}
-
-	@Test
-	fun `test logout - sucesso`() {
-		val response = controller.logout()
-
-		assert(response.statusCode == HttpStatus.OK)
-		assert(response.body == "Logout realizado com sucesso!")
+		assertEquals(campanhaAtualizada.idCampanha, resultado.idCampanha)
+		assertEquals(campanhaAtualizada.nome, resultado.nome)
 	}
 }
